52343525512798478e458298d7d6cf60014b4871
Alexei Sholik <alcosholik@gmail.com>
2014-07-01 04:18:19 +0300

    Custom key tests

--- test/encrypt_test.exs
+++ test/encrypt_test.exs
@@ -61,11 +61,32 @@ defmodule SaltieTest.Encrypt do
     end
   end
 
-  test "min length 20 large " do
+  test "min length 20 large" do
     s = Saltie.new(min_len: 20, alphabet: 'abcdefghijklmnop0')
     for {num, cipher} <- tests_from_fixture("min_length_20_large") do
       assert cipher == Saltie.encrypt(s, num)
       assert length(cipher) > 20
+    end
+  end
+
+  test "custom key 1" do
+    s = Saltie.new(key: 'hello world')
+    for {num, cipher} <- tests_from_fixture("key_hello_world") do
+      assert cipher == Saltie.encrypt(s, num)
+    end
+  end
+
+  test "custom key 2" do
+    s = Saltie.new(key: '123_-+EBNFarigatou')
+    for {num, cipher} <- tests_from_fixture("key_gibberish") do
+      assert cipher == Saltie.encrypt(s, num)
+    end
+  end
+
+  test "custom key large" do
+    s = Saltie.new(key: '-->secret key, no salt<--')
+    for {num, cipher} <- tests_from_fixture("custom_key_large") do
+      assert cipher == Saltie.encrypt(s, num)
     end
   end
 end
