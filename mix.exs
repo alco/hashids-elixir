7235c6613873561a536f29297f2bff11812cc277
Alexei Sholik <alcosholik@gmail.com>
2014-07-03 18:01:25 +0300

    Prepare for Hex packaging

--- mix.exs
+++ mix.exs
@@ -2,9 +2,29 @@ defmodule Saltie.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :saltie,
-     version: "0.3.0",
-     elixir: "~> 0.13.3 or ~> 0.14.0"]
+    [
+      app: :saltie,
+      version: "0.3.0",
+      elixir: "~> 0.13.3 or ~> 0.14.0",
+      description: description,
+      package: package,
+    ]
+  end
+
+  defp description do
+    "Saltie is a pseudo-encryption library primarily used for obfuscating " <>
+    "numerical identifiers to opaque strings."
+  end
+
+  defp package do
+    [
+      files: ["lib", "mix.exs", "README.md", "LICENSE"],
+      contributors: ["Alexei Sholik"],
+      licenses: ["MIT"],
+      links: %{
+        "GitHub" => "https://github.com/alco/saltie",
+      }
+    ]
   end
 
   # no deps
