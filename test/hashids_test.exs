b38296ffdbdcc1c29135a40327e0a5a6196e4ef6
Alexei Sholik <alcosholik@gmail.com>
2014-10-26 13:32:21 +0200

    Generate test cases directly from fixtures

--- test/hashids_test.exs
+++ test/hashids_test.exs
@@ -3,121 +3,20 @@ defmodule HashidsTest.Encode do
 
   import HashidsTest.Helpers
 
-  test "default salt" do
-    s = Hashids.new()
-    for {num, cipher} <- tests_from_fixture("default_salt") do
-      assert cipher == Hashids.encode(s, num)
-      assert [num] === Hashids.decode(s, cipher)
-    end
-  end
+  testcase_from_fixture "default_salt"
+  testcase_from_fixture "default_salt_list"
+  testcase_from_fixture "min_length_3"
+  testcase_from_fixture "min_length_20"
+  testcase_from_fixture "custom_salt_1"
+  testcase_from_fixture "custom_salt_2"
+  testcase_from_fixture "short_alphabet"
+  testcase_from_fixture "custom_alphabet"
+  testcase_from_fixture "long_alphabet"
+  testcase_from_fixture "mix_and_match"
 
-  test "default salt multinum" do
-    s = Hashids.new()
-    for {numlist, cipher} <- tests_from_fixture("default_salt_list") do
-      assert cipher == Hashids.encode(s, numlist)
-      assert numlist === Hashids.decode(s, cipher)
-    end
-  end
-
-  test "default salt large" do
-    s = Hashids.new()
-    for {num, cipher} <- tests_from_fixture_large("default_salt_large") do
-      assert cipher == Hashids.encode(s, num)
-      assert [num] === Hashids.decode(s, cipher)
-    end
-  end
-
-  test "short alphabet" do
-    s = Hashids.new(alphabet: 'abc1029384756XYZ')
-    for {num, cipher} <- tests_from_fixture("short_alphabet") do
-      assert cipher == Hashids.encode(s, num)
-      assert [num] === Hashids.decode(s, cipher)
-    end
-  end
-
-  test "short alphabet large" do
-    s = Hashids.new(alphabet: 'abc1029384756XYZ')
-    for {num, cipher} <- tests_from_fixture_large("short_alphabet_large") do
-      assert cipher == Hashids.encode(s, num)
-      assert [num] === Hashids.decode(s, cipher)
-    end
-  end
-
-  @long_one 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890~!@#$%^&*()_+"\][{}|;:/?.>,<あいうえおかきくけこさしすせそたちつてとまみむめもらちるれろはひふへほやゆよんをわなにぬねのабвгдежзийклмнопрстуфхцчшщьыъэюяАБВГДЕЖЗИЙКЛМНОПРСТУФХЦЧШЩЬЫЪЭЮЯ'
-
-  test "long alphabet" do
-    s = Hashids.new(alphabet: @long_one)
-    for {num, cipher} <- tests_from_fixture("long_alphabet") do
-      assert cipher == Hashids.encode(s, num)
-      assert [num] === Hashids.decode(s, cipher)
-    end
-  end
-
-  test "long alphabet large" do
-    s = Hashids.new(alphabet: @long_one)
-    for {num, cipher} <- tests_from_fixture_large("long_alphabet_large") do
-      assert cipher == Hashids.encode(s, num)
-      assert [num] === Hashids.decode(s, cipher)
-    end
-  end
-
-
-  test "min length 3" do
-    s = Hashids.new(min_len: 3)
-    for {num, cipher} <- tests_from_fixture("min_length_3") do
-      assert cipher == Hashids.encode(s, num)
-      assert length(cipher) >= 3
-      assert [num] === Hashids.decode(s, cipher)
-    end
-  end
-
-  test "min length 20" do
-    s = Hashids.new(min_len: 20)
-    for {num, cipher} <- tests_from_fixture("min_length_20") do
-      assert cipher == Hashids.encode(s, num)
-      assert length(cipher) == 20
-      assert [num] === Hashids.decode(s, cipher)
-    end
-  end
-
-  test "min length 20 large" do
-    s = Hashids.new(min_len: 20, alphabet: 'abcdefghijklmnop0')
-    for {num, cipher} <- tests_from_fixture_large("min_length_20_large") do
-      assert cipher == Hashids.encode(s, num)
-      assert length(cipher) > 20
-      assert [num] == Hashids.decode(s, cipher)
-    end
-  end
-
-  test "custom salt 1" do
-    s = Hashids.new(salt: 'hello world')
-    for {num, cipher} <- tests_from_fixture("custom_salt_1") do
-      assert cipher == Hashids.encode(s, num)
-      assert [num] === Hashids.decode(s, cipher)
-    end
-  end
-
-  test "custom salt 2" do
-    s = Hashids.new(salt: '123_-+EBNFarigatou')
-    for {num, cipher} <- tests_from_fixture("custom_salt_2") do
-      assert cipher == Hashids.encode(s, num)
-      assert [num] === Hashids.decode(s, cipher)
-    end
-  end
-
-  test "custom salt large" do
-    s = Hashids.new(salt: '-->secret key, no salt<--')
-    for {num, cipher} <- tests_from_fixture_large("custom_salt_large") do
-      assert cipher == Hashids.encode(s, num)
-      assert [num] == Hashids.decode(s, cipher)
-    end
-  end
-
-  test "mix and match" do
-    s = Hashids.new(salt: '123,./~_+', min_len: 5, alphabet: 'Aünîcø∂émädñèsSß')
-    for {nums, cipher} <- tests_from_fixture("mix_and_match") do
-      assert cipher == Hashids.encode(s, nums)
-      assert List.wrap(nums) === Hashids.decode(s, cipher)
-    end
-  end
+  testcase_from_fixture_large "default_salt_large"
+  testcase_from_fixture_large "custom_salt_large"
+  testcase_from_fixture_large "min_length_20_large"
+  testcase_from_fixture_large "short_alphabet_large"
+  testcase_from_fixture_large "long_alphabet_large"
 end
--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,6 +1,30 @@
 ExUnit.start()
 
 defmodule HashidsTest.Helpers do
+  defmacro testcase_from_fixture(name) do
+    testcase_from_fixture("base", name)
+  end
+
+  defmacro testcase_from_fixture_large(name) do
+    testcase_from_fixture("large", name)
+  end
+
+  defp testcase_from_fixture(basepath, name) do
+    {options, tests} =
+      File.read!(Path.join([__DIR__, "fixtures", "v1.0.0", basepath, name]))
+      |> parse_test_case()
+
+    quote do
+      test unquote(name) do
+        s = Hashids.new(unquote(options))
+        for {nums, encoded} <- unquote(tests) do
+          assert encoded == Hashids.encode(s, nums)
+          assert List.wrap(nums) === Hashids.decode(s, encoded)
+        end
+      end
+    end
+  end
+
   def tests_from_fixture_large(path) do
     File.read!(Path.join([__DIR__, "fixtures", "v1.0.0", "large", path]))
     |> tests_from_string()
@@ -9,6 +33,25 @@ defmodule HashidsTest.Helpers do
   def tests_from_fixture(path) do
     File.read!(Path.join([__DIR__, "fixtures", "v1.0.0", "base", path]))
     |> tests_from_string()
+  end
+
+  @header_re ~r/#\s+salt:\s+'([^']*)'\s+min_len:\s+(\d+)\s+alphabet:\s+(?:'([^']+)'|(<default>))$/
+
+  defp parse_test_case(str) do
+    [header, rest] = String.split(str, "\n", parts: 2)
+    options = case Regex.run(@header_re, header) do
+      [_, salt, min_len, alphabet, _] -> build_opts(salt, min_len, alphabet)
+      [_, salt, min_len, alphabet]    -> build_opts(salt, min_len, alphabet)
+    end
+    tests = tests_from_string(rest)
+    {options, tests}
+  end
+
+  defp build_opts(salt, min_len, alphabet) do
+    salt = String.to_char_list(salt)
+    len = String.to_integer(min_len)
+    alphabet_opt = if alphabet == "", do: [], else: [alphabet: String.to_char_list(alphabet)]
+    alphabet_opt ++ [salt: salt, min_len: len]
   end
 
   defp tests_from_string(str) do
@@ -21,21 +64,21 @@ defmodule HashidsTest.Helpers do
   end
 
   defp split_fields(str) do
-    # [1 2 3 4] <cipher>
+    # [1 2 3 4] <encoded>
     case Regex.run(~r/^\[([\d ]+)\]\s+(.+)$/, str) do
-      [_, numstr, cipher] ->
+      [_, numstr, encoded] ->
         numbers =
           numstr
           |> String.split(" ")
           |> Enum.map(&String.strip/1)
           |> Enum.reject(&(&1 == ""))
           |> Enum.map(&String.to_integer/1)
-        {numbers, String.to_char_list(cipher)}
+        {numbers, String.to_char_list(encoded)}
 
       _ ->
-        # <number> <cipher>
-        [numstr, cipher] = String.split(str, " ")
-        {String.to_integer(numstr), String.to_char_list(cipher)}
+        # <number> <encoded>
+        [numstr, encoded] = String.split(str, " ")
+        {String.to_integer(numstr), String.to_char_list(encoded)}
     end
   end
 end
