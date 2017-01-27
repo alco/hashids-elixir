fcda807dbd6ff015d9cf27f2d715dd498c889698
Alexei Sholik <alcosholik@gmail.com>
2017-01-27 14:34:41 +0100

    Support only the latest Elixir and Erlang versions

--- mix.exs
+++ mix.exs
@@ -7,7 +7,7 @@ defmodule Hashids.Mixfile do
     [
       app: :hashids,
       version: @version,
-      elixir: "~> 1.0",
+      elixir: "~> 1.4",
       deps: deps(),
       description: description(),
       package: package(),
